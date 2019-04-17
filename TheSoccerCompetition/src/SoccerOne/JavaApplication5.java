4194d51a038b527a9643fb1bc9e9174cb49ae905
Peter.Sigurdson <Peter.Sigurdson@123-Instructor.Tor.Loc>
2019-04-17 17:19:38 -0400

    comment

--- TheSoccerCompetition/src/SoccerOne/JavaApplication5.java
+++ TheSoccerCompetition/src/SoccerOne/JavaApplication5.java
@@ -4,11 +4,8 @@
  * and open the template in the editor.
  */
 package SoccerOne;
+import java.util.Random;
 
-/**
- *
- * @author Peter.Sigurdson
- */
 public class JavaApplication5 {
 
     public static void main(String[] args) {
@@ -24,16 +21,32 @@ class SoccerTeam{
 
 class TeamA extends SoccerTeam{
         Player Joe = new Player();
+        Player Moe = new Player();
+        Player Loe = new Player();
     }
 
 class TeamB extends SoccerTeam{
-
+        Player Brad = new Player();
+        Player Jad = new Player();
+        Player Lad = new Player();
     }
 
-class Player{
+        class Player{
+            Random rand = new Random();
+            public Player(){
+                this.RunningSpeed = rand.nextInt(100);
+                this.ScoringRank = rand.nextInt(100);
+    }
+    
     String PlayerName;
     int SeasonScore;
     int CareerScore;
     int RunningSpeed;
     int ScoringRank;
+}
+
+class Competition{
+    public void doCompetition(){
+        
+    }
 }
